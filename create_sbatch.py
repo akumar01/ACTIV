371f790941f28d53235c6c22578fb2216c538086
akumar01 <akumar.4db@gmail.com>
2019-02-04 12:00:16 -0800

    Started working on create_sbatch script

---
+++ create_sbatch.py
@@ -0,0 +1,124 @@
+import sys, os, errno
+from datetime import datetime
+import subprocess
+import shlex
+import pdb
+import itertools
+import glob
+import argparse
+import json
+import importlib
+import subprocess
+import h5py
+
+
+if __name__ == '__main__':
+	# Create and execute an sbatch script correlation calculations
+
+	parser = argparse.ArgumentParser()
+
+	# Name of data file to process
+	parser.add_argument('datfile', default = None)
+
+	# Name of folder to put the results in (by default, working in
+	# /global/project/projectdirs/m2043/ankit/ACTIV/correlations)
+
+	parser.add_argument('jobdir', default = None)
+
+	# c (channels): Calculate pairwise correlations between channels, averaged 
+	# over all frequencies
+
+	# fc (freq-channels): Calculate pairwise correlations between frequncies, 
+	# averaged over all channels
+
+	# fs (freq-sources): Calculate pairwise correlations between frequencies, 
+	# averaged over all sources
+
+	# s (sources): Calculate pairwise correlations between sources, averaged
+	# over all frequencies
+
+	# Level of parallelization: Each distinct job will calculate a single row
+	# of the corresponding correlation matrix
+
+	parser.add_argument('-c', '--channels', action = 'store_true')
+	parser.add_argument('-fc', '--freq-channels', action = 'store_true')
+	parser.add_argument('-fs', '--freq-sources', action = 'store_true')
+	parser.add_argument('-s', '--sources', action = 'store_true')
+
+	parser.add_argument('-t', '--time', action='store_const', const=12)
+
+	args = parser.parse_args()
+
+	script_dir = '/global/homes/a/akumar25'
+
+	data_path = '/global/project/projectdirs/m2043/ACTIV/stanfordEEG/DOEcollab_TMSEEGdata'
+
+	data_path = '%s/%s' % (data_path, args.datfile)
+
+	# ensure that data_file exists:
+	if not os.path.isfile(data_path):
+		raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), args.datfile)
+
+	# Get number of channels, sources, and frequencies:
+	with h5py.File(data_path, 'r') as f:
+		nchans = f['cfg_PAINT_cond']['channum'][0]
+		nsources = f['cfg_PAINT_cond']['sourcenum'][0]
+		nfreqs = f['cfg_PAINT_cond']['ERSPfreq'].size
+
+	# Number of rows of the correlation matrix for the different job types
+	jobnums = [nchans, nfreqs, nfreqs, nchans]
+
+	jobdir = '/global/project/projectdirs/m2043/akumar/ACTIV/correlations'
+
+	jobdir = '%s/%s' % (jobdir, args.jobdir)
+
+	if not os.path.exists(jobdir):
+		os.makedirs(jobdir)
+
+	# Log all details
+	log_file = open('%s/log.txt' % jobdir, 'w')
+	log_file.write('Jobs submitted at ' + "{:%H:%M:%S, %B %d, %Y}".format(datetime.now()) + '\n\n\n')
+
+	log_file.write('Data file Processed: %s\n' args.datfile)
+
+	for i, jobtype in enumerate(['channels', 'freq_channels', 'freq-sources', 'sources'])
+
+		if getattr(args, jobtype):
+
+			for i in range(jobnums[i]):
+				jobname = '%s%d' % (jobtype, i)
+				sbname = '%s/sbatch_%s.sh' % (jobdir, jobname)
+				with open(sbname, 'w') as sb:
+					# Arguments common across jobs
+					sb.write('#!/bin/bash\n')
+					sb.write('#SBATCH -q shared\n')
+					sb.write('#SBATCH -n 1\n')
+					sb.write('#SBATCH -t %s\n' % args.time)
+
+					sb.write('#SBATCH --job-name=%s\n' % jobname)
+					sb.write('#SBATCH --out=%s/%s.o\n' % (jobdir, jobname))
+					sb.write('#SBATCH --error=%s/%s.e\n' % (jobdir, jobname))
+					sb.write('#SBATCH --mail-user=ankit_kumar@berkeley.edu\n')
+					sb.write('#SBATCH --mail-type=FAIL\n')
+					# Load python and any other necessary modules
+					sb.write('module load python/3.6-anaconda-4.4\n')
+					# script(s) to actually run
+					if 'cori'.encode() in hostname:
+						sb.write('srun -C haswell python3  %s/%s %s %s %s %d' 
+							% (script_dir, data_path, jobtype, i))
+					else:
+						sb.write('srun python3  %s/%s %s' 
+							% (script_dir, job['script'], job['arg_file']))
+					sb.close()
+					
+				# Change permissions
+				os.system('chmod u+x %s' % sbname)
+				if not cmd_args.test:
+					if not cmd_args.first_only or i == 0:
+						# Submit the job
+						os.system('sbatch %s ' % sbname)
+
+	log_file.close()
+
+
+
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-import nump as np 
+import numpy as np 
+from scipy.stats import pearsonr
 import math
+import pdb
 
 
 # Orthogonalize the time series X and Y over time window T
@@ -17,6 +19,26 @@ def orth_series(X, Y, T):
 	return z 
 
 # Calculate the pairwise correlation between the frequency/sensor (source) pairs given by the first
-# 2 axes of data. Data shape is ()
+# 2 axes of data. Data shape is (time, freqs, sources)
 def calc_corr(data, orth = True):
+	
+	# Resulting correlation matrix should be across all pairs of sources and freqs (4D)
+	# (sources, sources', freqs, freqs')
+	corr_matrix = np.zeros((data.shape[1], data.shape[1], data.shape[2], data.shape[2]))
+	
+	# Iterate through each pair of (source, freq) pairs of time series, orthogonalize them,
+	# and then calculate their correlations
+	for i1 in range(data.shape[1]):
+		for i2 in range(data.shape[1]): 
+			for j1 in range(data.shape[2]):
+				for j2 in range(data.shape[2]):
+					if orth:
+						z = orth_series(data[:, i1, j1], data[:, i2, j2], 10)
+					else:
+						z = data[:, i2, j2]
+					r2, p = pearsonr(data[:, i1, j1], z)
+					if np.isnan(r2):
+						r2 = 1
+					corr_matrix[i1, i2, j1, j2] = r2
 
+	return corr_matrix
